695b3616d0b86db5bca2d843f75fb3c38c627897
danielcrubens <dcr.tecnologi@gmail.com>
2024-09-03 19:45:23 -0300

    feat: meta description  :white_check_mark:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR'
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Nuxt.Q - Interação Anônima através de Perguntas',
+      meta: [
+        { 
+          name: 'description', 
+          content: 'Nuxt.Q é uma plataforma de interação onde você pode criar salas para receber perguntas anônimas, gerenciar e marcar como lidas.'
+        },
+        {
+          name: 'keywords',
+          content: 'Nuxt.Q, perguntas anônimas, interação, Front-end, gerenciamento de perguntas'
+        }
+      ],
+    }
+  },
+
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts","@prisma/nuxt"],
