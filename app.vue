6833d7c2feda6b5a86829b6b8cc436397643c1bc
danielcrubens <dcr.tecnologi@gmail.com>
2024-09-26 16:14:01 -0300

    feat:  seo   :white_check_mark:

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
   <NuxtPage />
 </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Nuxt.Q',
+})
+
+useSeoMeta({
+  title: 'Nuxt.Q - Interação Anônima através de Perguntas',
+  ogTitle: 'Nuxt.Q - Interação Anônima através de Perguntas',
+  description: 'Plataforma interativa para criação de salas de perguntas anônimas',
+  ogDescription: 'Plataforma interativa para criação de salas de perguntas anônimas',
+})
+</script>
